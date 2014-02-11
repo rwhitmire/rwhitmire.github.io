3cd25ca0e93a83c1657162c8a1a112f1fd39c402
Ryan Whitmire <ryanw51@gmail.com>
2014-02-10 21:11:12 -0500

    update twitter feed to use node.js

--- js/tweets.js
+++ js/tweets.js
@@ -1,69 +1,54 @@
 $(function(){
-    $.support.cors = true;
-
-    var url = 'http://rwhitmire.azurewebsites.net/api/twitter';
-
-    var xhr = $.ajax({
-        type: 'get',
-    	url: url,
-        crossDomain: true,
-        dataType: 'text',
-        async: true,
-        cache: false
-    });
+    var xhr = $.getJSON('http://rwhitmire-api.herokuapp.com/twitter?callback=?');
 
     xhr.done(function(tweets){
-        tweets = JSON.parse(tweets);
-        console.log(tweets);
+        // tweets = JSON.parse(tweets);
 
-    	var $wrapper = $('#tweets');
-    	var $table = $('<table>').addClass('table');
-    	var $tr;
-    	var text;
-    	var date;
-    	var month;
-    	var day;
-    	var year;
-    	var dateStr;
-    	var monthLookup = {
-    		1: 'January',
-    		2: 'February',
-    		3: 'March',
-    		4: 'April',
-    		5: 'May',
-    		6: 'June',
-    		7: 'July',
-    		8: 'August',
-    		9: 'September',
-    		10: 'October',
-    		11: 'November',
-    		12: 'December'
-    	}
+        var $wrapper = $('#tweets');
+        var $table = $('<table>').addClass('table');
+        var $tr;
+        var text;
+        var date;
+        var month;
+        var day;
+        var year;
+        var dateStr;
+        var monthLookup = {
+            1: 'January',
+            2: 'February',
+            3: 'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'October',
+            11: 'November',
+            12: 'December'
+        };
 
-    	$.each(tweets, function(index, tweet){
-    		$tr = $('<tr>');
-    		$col1 = $('<td>');
-    		$col2 = $('<td>').addClass('text-right').addClass('date-col');
+        $.each(tweets, function(index, tweet){
+            $tr = $('<tr>');
+            $col1 = $('<td>');
+            $col2 = $('<td>').addClass('text-right').addClass('date-col');
 
-    		console.log(tweet);
+            text = tweet.text;
+            date = new Date(tweet.created_at);
 
-    		text = tweet.text;
-    		date = new Date(tweet.created_at);
+            day = date.getDate();
+            month = monthLookup[date.getMonth() + 1];
+            year = date.getFullYear();
 
+            dateStr = day + ' ' + month + ' ' + year;
 
-    		day = date.getDate();
-			month = monthLookup[date.getMonth() + 1];
-			year = date.getFullYear();
+            $col1.html(text);
+            $col2.html(dateStr);
+            $tr.append($col1).append($col2);
+            $table.append($tr);
+        });
 
-			dateStr = day + ' ' + month + ' ' + year;
-
-    		$col1.html(text);
-    		$col2.html(dateStr);
-    		$tr.append($col1).append($col2);
-		$table.append($tr);
-    	});
-
-    	$wrapper.append($table);
+        $wrapper.append($table);
     });
 
     xhr.fail(function(){
